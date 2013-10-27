eec4704ddfd7d248d69c2a5252ca683e6bade8e4
Johann Höchtl <johann.hoechtl@gmail.com>
2013-10-27 21:36:41 +0100

    Various additions
    * index.tpl: General: leave it to the partials to specify <div class="row-fluid">. Improves re-usability
    * main.html: get rid of place holder text and add some minimal useful explanations
    * DataSetListControl: new Controller for Dataset metadata details

--- static/js/ogdatwatcher.js
+++ static/js/ogdatwatcher.js
@@ -2,23 +2,25 @@ angular.module('ogdatanalysewebfrontend', ['ui.bootstrap', 'ngGrid', 'ajoslin.pr
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
 			when('/', {templateUrl: 'static/partials/main.html'}).
-			when('/statistic', {templateUrl: 'static/partials/statistic.html', controller: TaxonomyControll}).
+			when('/statistic', {templateUrl: 'static/partials/statistic.html', controller: TaxonomyControl}).
+			when('/dslist/:taxonomy/:subset', {templateUrl: 'static/partials/dslist.html', controller: DataSetListControl}).
 			otherwise({redirectTo: '/'});
 }]);
 
 var APIBASEURL = 'http://localhost:5100/v1/';
+var DATAPORTAL_APIBASEURL = 'http://www.data.gv.at/katalog/api/2/';
 
-function CollapseCtrl($scope) {
-	$scope.isCollapsed = false;
-}
-
-function TaxonomyControll($scope, $http, promiseTracker) {
+function TaxonomyControl($scope, $http, promiseTracker) {
 	
 	$scope.loadGrid = function(endpoint) {
 		var basetaxonomyurl = APIBASEURL + 'taxonomy/';
 		var fullurl = basetaxonomyurl + endpoint;
 
 		var get = $http.get(fullurl).success(function(data) {
+			// programmatically add a column 'href' which contains the link to the dataset list display page
+			for (var i = 0; i < data.length; i++) {
+				data[i].href = '#dslist/' + endpoint+ '/' + data[i].ID
+			}
 			$scope[endpoint] = data;
 		}).error(function(data, status, header) {
 			$scope[endpoint] = null;
@@ -27,11 +29,75 @@ function TaxonomyControll($scope, $http, promiseTracker) {
 		$scope[endpoint] = promiseTracker(endpoint);
 		$scope[endpoint].addPromise(get);
 	};
+		
+	var statistics = [
+		{source:'entities', columnDefs: [{field:'ID', displayName:'Einheit'}, {field:'Numsets', displayName:'Anzahl'}]},
+		{source:'versions', columnDefs: [{field:'ID', displayName:'Metadatenversion'}, {field:'Numsets', displayName:'Anzahl'}]},
+		{source:'toponyms', columnDefs: [{field:'ID', displayName:'Geographische Abdeckung'}, {field:'Numsets', displayName:'Anzahl'}]},
+		{source:'categories', columnDefs: [{field:'ID', displayName:'Kategorie'}, {field:'Numsets', displayName:'Anzahl'}]}
+	];
 	
-	var statistics = ['entities', 'versions', 'toponyms', 'categories'];
-	
+	// in the ng-grid, the element should be displayed as a link using the contents of the field 'href' as href
+	var linkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
+                       '  <a href="{{row.entity[\'href\']}}">{{row.getProperty(col.field)}}</a>' +
+                       '</div>';
+
 	statistics.map(function(item) {
-		$scope[item+'grid'] = { data: item};
-		$scope.loadGrid(item);
+		// The first and second item is a link to load the dataset list
+		item.columnDefs[0].cellTemplate = item.columnDefs[1].cellTemplate = linkCellTemplate;
+		
+		// set up the grid options
+		$scope[item.source+'grid'] = {
+			data: item.source,
+			showFooter: true,
+			multiSelect: false,
+			columnDefs: item.columnDefs
+		};
+		
+		$scope.loadGrid(item.source);
 	});
+}
+
+function DataSetListControl($scope, $http, $routeParams, promiseTracker) {
+
+	$scope.taxonomy = $routeParams.taxonomy;
+	$scope.subset = $routeParams.subset;
+	$scope.datataxonomygridselection = [];
+
+	$scope.loadGrid = function(which, subset) {
+		var basetaxonomyurl = APIBASEURL + 'datasets/taxonomy/' // + {which}/{subset} 
+		var fullurl = basetaxonomyurl + which + '/' + subset;
+
+		var get = $http.get(fullurl).success(function(data) {
+			// programmatically add a column 'href' which contains the link to the dataset list display page
+			for (var i = 0; i < data.length; i++) {
+				data[i].goto = '▶';
+				data[i].href = DATAPORTAL_APIBASEURL + 'rest/dataset/' + data[i].CKANID;
+			}
+			$scope.datataxonomydetails = data;
+		}).error(function(data, status, header) {
+			$scope.datataxonomydetails = null;
+			$scope.datataxonomydetailsalert = 'Error fetching data from ' + fullurl + ': ' + ', Status:' + status + ', Time: ' + Date.now();
+		});
+		$scope.datataxonomydetails = promiseTracker('datataxonomydetails');
+		$scope.datataxonomydetails.addPromise(get);
+	};
+
+	// in the ng-grid, the element should be displayed as a link using the contents of the field 'href' as href
+	var linkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
+		'  <a href="{{row.entity[\'href\']}}" target="_blank">{{row.getProperty(col.field)}}</a>' +
+		'</div>';
+
+	$scope.datataxonomydetailsgrid = {
+		data: 'datataxonomydetails',
+		showFooter: true,
+		multiSelect: true,
+		selectedItems: $scope.datataxonomygridselection,
+		columnDefs: [
+			{field:'goto', displayName:'', cellTemplate:linkCellTemplate, width:20},
+			{field:'Description', displayName:'Beschreibung'}
+		]
+	};
+
+	$scope.loadGrid($scope.taxonomy, $scope.subset);
 }
--- templates/index.tpl
+++ templates/index.tpl
@@ -2,7 +2,7 @@
 <html ng-app="ogdatanalysewebfrontend">
 	<head>
 		<meta charset="UTF-8"/>
-		<title>A title</title>
+		<title>Open Government Data Österreich - Metadatenqualitätsportal</title>
 		<!--link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet"/-->
 		<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet"/>
 		<link href="./static/css/ng-grid.min.css" rel="stylesheet"/>
@@ -15,20 +15,12 @@
 				<div class="span12">
 					<div class="page-header">
 						<h1>
-							Example page header <small>Subtext for header</small>
+							Open Government Data Österreich <small>Statistik, Konsistenzchecks und Entwicklung</small>
 						</h1>
 					</div>
-					<div>
-						<label>Name:</label>
-						<input type="text" ng-model="yourName" placeholder="Enter a name here"/>
-						<hr/>
-						<h1>Hello {{yourName}}!</h1>
-					</div>			
 				</div>
 			</div>
-			<div class="row-fluid">
-				<div ng-view></div>
-			</div>
+			<div ng-view></div>
 			<div class="row-fluid">
 				<div class="span12">
 					<p>Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
