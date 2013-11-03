4b3554febf5612c69b8756482cb450ca5d4cf0a0
Johann Höchtl <johann.hoechtl@gmail.com>
2013-11-03 18:10:15 +0100

    provide some helpful links on the details view for metadata descriptions

--- static/js/ogdatwatcher.js
+++ static/js/ogdatwatcher.js
@@ -11,7 +11,7 @@ var APIBASEURL = 'http://localhost:5100/v1/';
 var DATAPORTAL_APIBASEURL = 'http://www.data.gv.at/katalog/api/2/';
 
 function TaxonomyControl($scope, $http, promiseTracker) {
-	
+
 	$scope.loadGrid = function(endpoint) {
 		var basetaxonomyurl = APIBASEURL + 'taxonomy/';
 		var fullurl = basetaxonomyurl + endpoint;
@@ -29,23 +29,23 @@ function TaxonomyControl($scope, $http, promiseTracker) {
 		$scope[endpoint] = promiseTracker(endpoint);
 		$scope[endpoint].addPromise(get);
 	};
-		
+
 	var statistics = [
 		{source:'entities', columnDefs: [{field:'ID', displayName:'Veröffentlichende Stelle'}, {field:'Numsets', displayName:'Anzahl'}]},
 		{source:'versions', columnDefs: [{field:'ID', displayName:'Metadatenversion'}, {field:'Numsets', displayName:'Anzahl'}]},
 		{source:'toponyms', columnDefs: [{field:'ID', displayName:'Geographische Abdeckung'}, {field:'Numsets', displayName:'Anzahl'}]},
 		{source:'categories', columnDefs: [{field:'ID', displayName:'Kategorie'}, {field:'Numsets', displayName:'Anzahl'}]}
 	];
-	
+
 	// in the ng-grid, the element should be displayed as a link using the contents of the field 'href' as href
 	var linkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
-                       '  <a href="{{row.entity[\'href\']}}">{{row.getProperty(col.field)}}</a>' +
-                       '</div>';
+		'  <a href="{{row.entity[\'href\']}}">{{row.getProperty(col.field)}}</a>' +
+		'</div>';
 
 	statistics.map(function(item) {
 		// The first and second item is a link to load the dataset list
 		item.columnDefs[0].cellTemplate = item.columnDefs[1].cellTemplate = linkCellTemplate;
-		
+
 		// set up the grid options
 		$scope[item.source+'grid'] = {
 			data: item.source,
@@ -53,21 +53,59 @@ function TaxonomyControl($scope, $http, promiseTracker) {
 			multiSelect: false,
 			columnDefs: item.columnDefs
 		};
-		
+
 		$scope.loadGrid(item.source);
 	});
 }
 
 function DataSetListControl($scope, $http, $routeParams, $sanitize, promiseTracker) {
 
+	$scope.MetadataDescriptionUrl = function(version, id) {
+
+		var url;
+
+		if (version.indexOf("2.0") > 0 || version.indexOf("2.1") > 0) {
+			url = "http://htmlpreview.github.io/?https://github.com/the42/ogdat/blob/master/ppogdatspec/ogdat_spec-2.1.html";
+		} else if (version.indexOf("2.2") > 0) {
+			// url = "";  // set url to new version???
+		} else {
+			// url = "";
+		}
+
+		if (url &&  id) {
+			url += "#ID.desc." + id;
+		}
+
+		return url;
+	};
+
+	$scope.loadGrid = function(which, subset) {
+		var basetaxonomyurl = APIBASEURL + 'datasets/taxonomy/' // + {which}/{subset}
+		var fullurl = basetaxonomyurl + which + '/' + subset;
+
+		var get = $http.get(fullurl).success(function(data) {
+			// programmatically add a column 'href' which contains the link to the dataset list display page
+			for (var i = 0; i < data.length; i++) {
+				data[i].goto = '▶';
+				data[i].href = DATAPORTAL_APIBASEURL + 'rest/dataset/' + data[i].CKANID;
+			}
+			$scope.datataxonomydetails = data;
+		}).error(function(data, status, header) {
+			$scope.datataxonomydetails = null;
+			$scope.datataxonomydetailsalert = 'Error fetching data from ' + fullurl + ': ' + ', Status:' + status + ', Time: ' + Date.now();
+		});
+		$scope.datataxonomydetails = promiseTracker('datataxonomydetails');
+		$scope.datataxonomydetails.addPromise(get);
+	};
+
 	$scope.contextheading = "";
 	$scope.datataxonomygridselection = [];
-	
+
 	$scope.taxonomy = $routeParams.taxonomy;
 	$scope.subset = $routeParams.subset;
-	
+
 	$scope.JSONDATASETBASEURL = DATAPORTAL_APIBASEURL + 'rest/dataset/';
-	
+
 	switch($scope.taxonomy) {
 		case "entities":
 			$scope.contextheading = "Veröffentlichende Stelle";
@@ -85,25 +123,6 @@ function DataSetListControl($scope, $http, $routeParams, $sanitize, promiseTrack
 			$scope.contextheading = "";
 			break;
 	}
-
-	$scope.loadGrid = function(which, subset) {
-		var basetaxonomyurl = APIBASEURL + 'datasets/taxonomy/' // + {which}/{subset} 
-		var fullurl = basetaxonomyurl + which + '/' + subset;
-
-		var get = $http.get(fullurl).success(function(data) {
-			// programmatically add a column 'href' which contains the link to the dataset list display page
-			for (var i = 0; i < data.length; i++) {
-				data[i].goto = '▶';
-				data[i].href = DATAPORTAL_APIBASEURL + 'rest/dataset/' + data[i].CKANID;
-			}
-			$scope.datataxonomydetails = data;
-		}).error(function(data, status, header) {
-			$scope.datataxonomydetails = null;
-			$scope.datataxonomydetailsalert = 'Error fetching data from ' + fullurl + ': ' + ', Status:' + status + ', Time: ' + Date.now();
-		});
-		$scope.datataxonomydetails = promiseTracker('datataxonomydetails');
-		$scope.datataxonomydetails.addPromise(get);
-	};
 
 	// in the ng-grid, the element should be displayed as a link using the contents of the field 'href' as href
 	var linkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
